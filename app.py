3163b66a203456c84dc8bce6cc19521aa7276269
Galen Sho Sato <galen.sato@gmail.com>
2023-11-19 18:00:38 -0500

    Added route to assign draft picks to users; set up tests for drafting

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, make_response, session as browser_session
-from server import db, migrate
+from server import db, migrate, User, Week, Year, Team, DraftPick, UserDraftPick
 
 
 app = Flask(__name__)
@@ -14,6 +14,32 @@ migrate.init_app(app, db)
 @app.route("/")
 def home():
     return "Wins Pool API"
+
+
+# Routes for Draft Feature
+@app.route("/assign_draft_picks", methods=["POST"])
+# JSON request should look like:
+# {"name": user,
+#  "year": year,
+#  "draftPicks: [1,2,3]"}
+def assign_draft_picks():
+    data = request.get_json()
+    user_name = data.get("name")
+    year_name = data.get("year")
+    user = User.query.filter(User.name == user_name).first()
+    year = Year.query.filter(Year.name == year_name).first()
+    draft_picks = data.get("draftPicks")
+
+    for pick in draft_picks:
+        draft_pick = DraftPick.query.filter(DraftPick.pick_number == pick).first()
+        new_draft_pick_assignment = UserDraftPick(
+            user=user, year=year, draft_pick=draft_pick
+        )
+        db.session.add(new_draft_pick_assignment)
+        db.session.commit()
+    return make_response(
+        {"Success": f"Draft picks were successfully assigned to {user_name}"}, 201
+    )
 
 
 if __name__ == "__main__":
--- server/tests/draft_test.py
+++ server/tests/draft_test.py
@@ -1,12 +1,20 @@
-# import pytest
-# import os
-# import sys
+import pytest
+import os
+import sys
 
-# project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-# sys.path.append(project_root)
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+sys.path.append(project_root)
 
-# from app import app
-# from server.extensions import db
-# from server.models import User, Year, Team, DraftPick, UserDraftPick
-# from flask_testing import TestCase
-# import json
+from app import app
+from server.extensions import db
+from server.models import User, Year, Team, DraftPick, UserDraftPick
+
+
+class TestDraftAssignments:
+    """Tests related to draft assignments"""
+
+    def test_draft_assignments_to_user(self):
+        pass
+
+    def test_team_assignments_to_user(self):
+        pass
