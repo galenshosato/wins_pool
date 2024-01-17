558a36fe3d00141c19082dc82f5acc4da76e7ca5
Galen Sho Sato <galen.sato@gmail.com>
2024-01-16 20:36:03 -0600

    Added a seeder file for teams, weeks, and years 2023/2024

---
+++ server/seeder.py
@@ -0,0 +1,65 @@
+from app import app
+from extensions import db
+from models import Team, Week, Year
+
+if __name__ == "__main__":
+    with app.app_context():
+        week_array = []
+        for n in range(1, 19):
+            new_week = Week(week_number=n)
+            week_array.append(new_week)
+        db.session.add_all(week_array)
+        db.session.commit()
+
+        year_2023 = Year(year=2023)
+        year_2024 = Year(year=2024)
+        db.session.add_all([year_2023, year_2024])
+        db.session.commit()
+
+        # AFC East
+        patriots = Team(team_name="Patriots", color="002a5c", alt_color="c60c30")
+        bills = Team(team_name="Bills", color="00338d", alt_color="d50a0a")
+        dolphins = Team(team_name="Dolphins", color="008e97", alt_color="fc4c02")
+        jets = Team(team_name="Jets", color="115740", alt_color="ffffff")
+
+        # AFC North
+        ravens = Team(team_name="Ravens", color="29126f", alt_color="000000")
+        steelers = Team(team_name="Steelers", color="000000", alt_color="ffb612")
+        browns = Team(team_name="Browns", color="472a08", alt_color="ff3c00")
+        bengals = Team(team_name="Bengals", color="fb4f14", alt_color="000000")
+
+        # AFC West
+        chiefs = Team(team_name="Chiefs", color="e31837", alt_color="ffb612")
+        broncos = Team(team_name="Broncos", color="0a2343", alt_color="fc4c02")
+        raiders = Team(team_name="Raiders", color="000000", alt_color="a5acaf")
+        chargers = Team(team_name="Chargers", color="0080c6", alt_color="ffc20e")
+
+        # AFC South
+        jaguars = Team(team_name="Jaguars", color="007487", alt_color="d7a22a")
+        texans = Team(team_name="Texans", color="00143f", alt_color="c41230")
+        colts = Team(team_name="Colts", color="003b75", alt_color="ffffff")
+        titans = Team(team_name="Titans", color="4b92db", alt_color="002a5c")
+
+        # NFC East
+        giants = Team(team_name="Giants", color="003c7f", alt_color="c9243f")
+        cowboys = Team(team_name="Cowboys", color="002a5c", alt_color="b0b7bc")
+        eagles = Team(team_name="Eagles", color="06424d", alt_color="000000")
+        commanders = Team(team_name="Commanders", color="5a1414", alt_color="ffb612")
+
+        # NFC North
+        packers = Team(team_name="Packers", color="204e32", alt_color="ffb612")
+        bears = Team(team_name="Bears", color="0b1c3a", alt_color="e64100")
+        vikings = Team(team_name="Vikings", color="4f2683", alt_color="ffc62f")
+        lions = Team(team_name="Lions", color="0076b6", alt_color="bbbbbb")
+
+        # NFC West
+        niners = Team(team_name="49ers", color="aa0000", alt_color="b3995d")
+        seahawks = Team(team_name="Seahawks", color="002a5c", alt_color="69be28")
+        rams = Team(team_name="Rams", color="003594", alt_color="ffd100")
+        cardinals = Team(team_name="Cardinals", color="a4113e", alt_color="ffffff")
+
+        # NFC South
+        panthers = Team(team_name="Panthers", color="0085ca", alt_color="000000")
+        buccaneers = Team(team_name="Buccaneers", color="bd1c36", alt_color="3e3a35")
+        saints = Team(team_name="Saints", color="d3bc8d", alt_color="000000")
+        falcons = Team(team_name="Falcons", color="a71930", alt_color="000000")
