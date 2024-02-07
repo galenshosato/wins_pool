2eb4c2477fc55f3cb693d3553360d662e793abb5
Galen Sho Sato <galen.sato@gmail.com>
2024-02-06 16:26:45 -0800

    Added function to update wins from ESPN api

--- server/functions/update_functions.py
+++ server/functions/update_functions.py
@@ -10,8 +10,16 @@ def calculate_strength_of_schedule(wins, losses, ties):
 
 # Returns an array of the winners for the week from the ESPN API
 def get_winners_from_ESPN(year, week):
+    winner_array = []
     espn_response = requests.get(
         f"https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?dates={year}&seasontype=2&week={week}"
     )
     espn_object = json.loads(espn_response.text)
-    # TODO: Go through each game and find the winner, then store in an array and return
+    games = espn_object["events"]
+    for game in games:
+        teams = game["competitions"][0]["competitors"]
+        for team in teams:
+            if team["winner"] == True:
+                name = team["team"]["name"]
+                winner_array.append(name)
+    return winner_array
