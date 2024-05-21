2a9bec6c1f5703ca2b90db2be1ff33088b9e90c2
Galen Sato <galen.sato@gmail.com>
2024-05-21 16:07:49 -0400

    added beginning of java functionality

---
+++ jserver/src/main/java/gssato/wins_pool/App.java
@@ -0,0 +1,13 @@
+package gssato.wins_pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
